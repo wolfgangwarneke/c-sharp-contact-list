be3535946db8c928a8ac7b4ea940603ece115703
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-07-24 23:16:39 -0700

    add Contact class private variables and constructor

--- Objects/Contact.cs
+++ Objects/Contact.cs
@@ -1,19 +1,23 @@
-// using System;
-// using System.Collections.Generic;
-//
-// namespace ContactList.Objects
-// {
-//   public class Contact
-//   {
-//     private static List<Contact> contactInstances;
-//     private string _firstName;
-//     private string _lastName;
-//     private string _email;
-//     private string _phoneNumber;
-//
-//     public Contact(string firstName, string lastName, string email, string phoneNumber)
-//     {
-//
-//     }
-//   }
-// }
+using System;
+using System.Collections.Generic;
+
+namespace ContactList.Objects
+{
+  public class Contact
+  {
+    private static List<Contact> contactInstances;
+    private string _firstName;
+    private string _lastName;
+    private string _email;
+    private string _phoneNumber;
+    //phone number is string so user can choose formatting
+
+    public Contact(string firstName, string lastName, string email, string phoneNumber)
+    {
+      _firstName = firstName;
+      _lastName = lastName;
+      _email = email;
+      _phoneNumber = phoneNumber;
+    }
+  }
+}
