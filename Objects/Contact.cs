0a18f1363167a503cea7d706fae58bba2470b711
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-07-24 23:38:21 -0700

    add getters and setters for contact private variables first name, last name, email, and phone number

--- Objects/Contact.cs
+++ Objects/Contact.cs
@@ -20,5 +20,43 @@ namespace ContactList.Objects
       _phoneNumber = phoneNumber;
       contactInstances.Add(this);
     }
+
+    public static List<Contact> GetAllContacts()
+    {
+      return contactInstances;
+    }
+
+    public string GetFirstName()
+    {
+      return _firstName;
+    }
+    public void SetFirstName(string firstName)
+    {
+      _firstName = firstName;
+    }
+    public string GetLastName()
+    {
+      return _lastName;
+    }
+    public void SetLastName(string lastName)
+    {
+      _lastName = lastName;
+    }
+    public string GetEmail()
+    {
+      return _email;
+    }
+    public void SetEmail(string email)
+    {
+      _email = email;
+    }
+    public string GetPhoneNumber()
+    {
+      return _phoneNumber;
+    }
+    public void SetPhoneNumber(string phoneNumber)
+    {
+      _phoneNumber = phoneNumber;
+    }
   }
 }
