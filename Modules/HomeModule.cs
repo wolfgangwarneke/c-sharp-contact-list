53eac1c6f8f6fc527a701cd004c2e2bd8042c6ef
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-07-24 23:07:19 -0700

    create initial filetree

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,16 @@
+using Nancy;
+using ContactList.Objects;
+using System.Collections.Generic;
+
+namespace ContactList
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+    }
+  }
+}
---
+++ Objects/Contact.cs
@@ -0,0 +1,19 @@
+// using System;
+// using System.Collections.Generic;
+//
+// namespace ContactList.Objects
+// {
+//   public class Contact
+//   {
+//     private static List<Contact> contactInstances;
+//     private string _firstName;
+//     private string _lastName;
+//     private string _email;
+//     private string _phoneNumber;
+//
+//     public Contact(string firstName, string lastName, string email, string phoneNumber)
+//     {
+//
+//     }
+//   }
+// }
